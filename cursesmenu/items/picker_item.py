e725a6071e32b9dc7649a90aacc2bd7ce41731c2
CraigBusch <CraigRBusch@gmail.com>
2017-04-21 18:59:50 -0400

    Remove unnecessary method from picker_item.py
    Index is no longer a variable within PickerItem.

--- cursesmenu/items/picker_item.py
+++ cursesmenu/items/picker_item.py
@@ -6,12 +6,12 @@ class PickerItem(MenuItem):
     The item type used in :class:`cursesmenu.SelectionMenu`
     """
 
-    def __init__(self, text, selected, menu=None):
+    def __init__(self, text, menu=None):
         """
         :ivar int index: The index of this item in the list used to initialize the :class:`cursesmenu.PickerMenu`
         """
         super(PickerItem, self).__init__(text=text, menu=menu, should_exit=True)
-        self.selected = selected
+        self.selected = False
         self.text = "[ ] " + self.text
 
     def toggle_selection(self):
@@ -24,10 +24,3 @@ class PickerItem(MenuItem):
         else:
             self.selected = True
             self.text = self.text.replace("[ ] ", "[X] ")
-
-    def get_return(self):
-        """
-        :return: The index of this item in the list of strings
-        :rtype: boolean
-        """
-        return self.selected
