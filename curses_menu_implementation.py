633e1f7180874411f87f9283051302321c711ef2
CraigBusch <CraigRBusch@gmail.com>
2017-04-21 19:13:19 -0400

    Update curses_menu_implementation.py
    Add PickerMenu example. I specified which submenu was the selection menu
    and which was the picker menu.

--- curses_menu_implementation.py
+++ curses_menu_implementation.py
@@ -11,7 +11,7 @@ menu = CursesMenu("Title", "Subtitle")
 menu_item = MenuItem("Menu Item")
 
 # A FunctionItem runs a Python function when selected
-function_item = FunctionItem("Call a Python function", input, ["Enter an input"])
+function_item = FunctionItem("Call a Python function", input, ["Enter an input: "])
 
 # A CommandItem runs a console command
 command_item = CommandItem("Run a console command",  "touch hello.txt")
@@ -21,13 +21,21 @@ selection_menu = SelectionMenu(["item1", "item2", "item3"])
 
 # A SubmenuItem lets you add a menu (the selection_menu above, for example)
 # as a submenu of another menu
-submenu_item = SubmenuItem("Submenu item", selection_menu, menu)
+submenu_item = SubmenuItem("Submenu Item (Selection Menu)", selection_menu, menu)
+
+# A SelectionMenu constructs a menu from a list of strings
+picker_menu = PickerMenu(["file1", "file2", "file3"])
+
+# A SubmenuItem lets you add a menu (the selection_menu above, for example)
+# as a submenu of another menu
+picker_submenu_item = SubmenuItem("Submenu Item (Picker Menu)", picker_menu, menu)
 
 # Once we're done creating them, we just add the items to the menu
 menu.append_item(menu_item)
 menu.append_item(function_item)
 menu.append_item(command_item)
 menu.append_item(submenu_item)
+menu.append_item(picker_submenu_item)
 
 # Finally, we call show to show the menu and allow the user to interact
 menu.show()
