8a7efeaf25baf1f08b50559b78d6a30d5ccb2193
CraigBusch <CraigRBusch@gmail.com>
2017-04-20 22:04:56 -0400

    Fix __init__.py files
    Imports were not working. Add new imports to __init__.py files.

--- cursesmenu/__init__.py
+++ cursesmenu/__init__.py
@@ -1,7 +1,8 @@
 from .curses_menu import CursesMenu
 from .curses_menu import clear_terminal
 from .selection_menu import SelectionMenu
+from .picker_menu import PickerMenu
 from . import items
 from .version import __version__
 
-__all__ = ['CursesMenu', 'SelectionMenu', 'items', 'clear_terminal']
+__all__ = ['CursesMenu', 'SelectionMenu', 'PickerMenu', 'items', 'clear_terminal']
--- cursesmenu/items/__init__.py
+++ cursesmenu/items/__init__.py
@@ -5,5 +5,13 @@ from .command_item import CommandItem
 from .function_item import FunctionItem
 from .submenu_item import SubmenuItem
 from .selection_item import SelectionItem
+from .picker_item import PickerItem
 
-__all__ = ['CommandItem', 'ExitItem', 'ExternalItem', 'FunctionItem', 'MenuItem', 'SelectionItem', 'SubmenuItem']
+__all__ = ['CommandItem',
+           'ExitItem',
+           'ExternalItem',
+           'FunctionItem',
+           'MenuItem',
+           'SelectionItem',
+           'PickerItem',
+           'SubmenuItem']
