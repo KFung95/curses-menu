032858c7582f68fae2eb9ce34c60788791a78779
KFung95 <Kevin.Fung.1995@gmail.com>
2017-04-23 21:38:15 -0400

    Add multiple pairings for Color
    Linke 367 should be changed from curses.A_NORMAL to
    curses.color_pair(<pair number>)

--- cursesmenu/curses_menu.py
+++ cursesmenu/curses_menu.py
@@ -184,6 +184,8 @@ class CursesMenu(object):
         if scr is not None:
             CursesMenu.stdscr = scr
         self.screen = curses.newpad(len(self.items) + 6, CursesMenu.stdscr.getmaxyx()[1])
+        curses.initscr();
+        curses.start_color()
         self._set_up_colors()
         curses.curs_set(0)
         CursesMenu.stdscr.refresh()
@@ -354,6 +356,13 @@ class CursesMenu(object):
 
     def _set_up_colors(self):
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(5, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        curses.init_pair(6, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+        curses.init_pair(7, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(8, curses.COLOR_WHITE, curses.COLOR_BLACK)
         self.highlight = curses.color_pair(1)
         self.normal = curses.A_NORMAL
 
