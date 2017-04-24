36feb5e1511c4c7a4345d642e3a378b45035f792
CraigBusch <CraigRBusch@gmail.com>
2017-04-23 21:29:09 -0400

    Remove unused method in PickerMenu
    Remove unused method left over from SelectionMenu.

--- cursesmenu/picker_menu.py
+++ cursesmenu/picker_menu.py
@@ -15,22 +15,6 @@ class PickerMenu(CursesMenu):
         for option in options:
             self.append_item(PickerItem(option, self))
 
-    @classmethod
-    def get_selection(cls, options, title, subtitle, exit_option=False, _menu=None):
-        """
-        Single-method way of getting a selection out of a list of strings
-
-        :param list[str] strings: the list of string used to build the menu
-        :param list _menu: should probably only be used for testing, pass in a list and the created menu used \
-        internally by the method will be appended to it
-        """
-        menu = cls(options, title, subtitle, exit_option)
-        if _menu is not None:
-            _menu.append(menu)
-        menu.show()
-        menu.join()
-        return menu.selected_option
-
     def gather_selections(self):
         """
         A method that returns all of the base text values of selected options in a list
