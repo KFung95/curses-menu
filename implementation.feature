2e14b4b1aa06b0d94c55ba68e24672f29b6a4983
CraigBusch <CraigRBusch@gmail.com>
2017-04-28 22:34:40 -0400

    Update feature documentation and implementation feature
    *Update feature_documentation.txt to explain how to run behave
    *Change tests for implementation.feature
    *Change implementation_steps.py to match new tests

--- implementation.feature
+++ implementation.feature
@@ -7,37 +7,27 @@ Feature: Implementing curses-menu
      When we implement a CursesMenu with the title "Test Menu" and the subtitle "Subtitle"
      Then we will have an empty curses menu with a title
 
-  Scenario: Create a PickerMenu
-    Given we have curses-menu installed
-      When we implement a PickerMenu with a list of selections
-      Then we will have a populated PickerMenu
-
-  Scenario: Create a SelectionMenu
-    Given we have curses-menu installed
-      When we implement a SelectionMenu with a list of selections
-      Then we will have a populated SelectionMenu
-
   Scenario: Create MenuItem named "MenuItem" in a curses menu
     Given we have a curse menu created
       When we implement a MenuItem
       Then we will have a curse menu containing a MenuItem
 
   Scenario: Create FunctionItem in a curses menu
-    Given we have a curse menu created
+    Given we have a curse menu created with a MenuItem
       When we implement a FunctionItem
       Then we will have a curse menu containing a FunctionItem
 
   Scenario: Create CommandItem in a curses menu
-    Given we have a curse menu created
+    Given we have a curse menu created with a MenuItem and a FunctionItem
       When we implement a CommandItem
       Then we will have a curse menu containing a CommandItem
 
   Scenario: Create selection SubmenuItem in a curses menu
-    Given we have a curse menu created
+    Given we have a curse menu created with a MenuItem and a FunctionItem and a CommandItem
       When we implement a selection SubmenuItem
       Then we will have a curse menu containing a SubmenuItem that contains a SelectionMenu
 
   Scenario: Create picker SubmenuItem in a curses menu
-    Given we have a curse menu created
+    Given we have a curse menu created with a MenuItem and a FunctionItem and a CommandItem and a selection SubmenuItem
       When we implement a picker SubmenuItem
       Then we will have a curse menu containing a SubmenuItem that contains a PickerMenu
--- steps/implementation_steps.py
+++ steps/implementation_steps.py
@@ -12,42 +12,30 @@ def before_all(context):
     context.selection_menu = None
     context.selection_submenu_item = None
     context.picker_submenu_item = None
+    context.menu = None
+    context.selection_menu = None
+    context.picker_menu = None
 
 
 @given("we have curses-menu installed")
 def step_impl(context):
-    context.list = ["item1", "item2", "item3"]
-    context.menu = None
-    context.selection_menu = None
-    context.picker_menu = None
-    pass
-
-
-@given("we have a curse menu created")
-def step_impl(context):
-    context.menu = CursesMenu("Test Menu", "Subtitle")
-    context.menu.show()
     pass
 
 
 @when('we implement a CursesMenu with the title "Test Menu" and the subtitle "Subtitle"')
 def step_impl(context):
     context.menu = CursesMenu("Test Menu", "Subtitle")
-    context.menu.show()
     pass
 
 
-@when("we implement a PickerMenu with a list of selections")
+@then("we will have an empty curses menu with a title")
 def step_impl(context):
-    assert len(context.list) != 0
-    context.picker_menu = PickerMenu(context.list)
-    pass
+    assert context.menu is not None
 
 
-@when("we implement a SelectionMenu with a list of selections")
+@given("we have a curse menu created")
 def step_impl(context):
-    assert len(context.list) != 0
-    context.selection_menu = SelectionMenu(context.list)
+    context.menu = CursesMenu("Test Menu", "Subtitle")
     pass
 
 
@@ -55,7 +43,20 @@ def step_impl(context):
 def step_impl(context):
     context.menu_item = MenuItem("NORMAL", "Menu Item")
     context.menu.append_item(context.menu_item)
-    context.menu.draw()
+    pass
+
+
+@then("we will have a curse menu containing a MenuItem")
+def step_impl(context):
+    assert context.menu is not None
+    assert context.menu_item is not None
+
+
+@given('we have a curse menu created with a MenuItem')
+def step_impl(context):
+    context.menu = CursesMenu("Test Menu", "Subtitle")
+    context.menu_item = MenuItem("NORMAL", "Menu Item")
+    context.menu.append_item(context.menu_item)
     pass
 
 
@@ -63,7 +64,23 @@ def step_impl(context):
 def step_impl(context):
     context.function_item = FunctionItem("NORMAL", "Call a Python function", input, ["Enter an input"])
     context.menu.append_item(context.function_item)
-    context.menu.draw()
+    pass
+
+
+@then("we will have a curse menu containing a FunctionItem")
+def step_impl(context):
+    assert context.menu is not None
+    assert context.menu_item is not None
+    assert context.function_item is not None
+
+
+@given('we have a curse menu created with a MenuItem and a FunctionItem')
+def step_impl(context):
+    context.menu = CursesMenu("Test Menu", "Subtitle")
+    context.menu_item = MenuItem("NORMAL", "Menu Item")
+    context.menu.append_item(context.menu_item)
+    context.function_item = FunctionItem("NORMAL", "Call a Python function", input, ["Enter an input"])
+    context.menu.append_item(context.function_item)
     pass
 
 
@@ -71,80 +88,77 @@ def step_impl(context):
 def step_impl(context):
     context.command_item = CommandItem("NORMAL", "Run a console command",  "ipconfig /all")
     context.menu.append_item(context.command_item)
-    context.menu.draw()
+    pass
+
+
+@then("we will have a curse menu containing a CommandItem")
+def step_impl(context):
+    assert context.menu is not None
+    assert context.menu_item is not None
+    assert context.function_item is not None
+    assert context.command_item is not None
+
+
+@given('we have a curse menu created with a MenuItem and a FunctionItem and a CommandItem')
+def step_impl(context):
+    context.menu = CursesMenu("Test Menu", "Subtitle")
+    context.menu_item = MenuItem("NORMAL", "Menu Item")
+    context.menu.append_item(context.menu_item)
+    context.function_item = FunctionItem("NORMAL", "Call a Python function", input, ["Enter an input"])
+    context.menu.append_item(context.function_item)
+    context.command_item = CommandItem("NORMAL", "Run a console command",  "ipconfig /all")
+    context.menu.append_item(context.command_item)
     pass
 
 
 @when("we implement a selection SubmenuItem")
 def step_impl(context):
-    context.selection_submenu_item = SubmenuItem("NORMAL", "Submenu item", context.selection_menu, context.menu)
+    context.selection_menu = SelectionMenu(["item1", "item2", "item3"])
+    context.selection_submenu_item = SubmenuItem("NORMAL", "Submenu item", context.selection_menu)
     context.menu.append_item(context.selection_submenu_item)
-    context.menu.draw()
+    pass
+
+
+@then("we will have a curse menu containing a SubmenuItem that contains a SelectionMenu")
+def step_impl(context):
+    assert context.menu is not None
+    assert context.menu_item is not None
+    assert context.function_item is not None
+    assert context.command_item is not None
+    assert context.selection_menu is not None
+    assert context.selection_submenu_item is not None
+
+
+@given('we have a curse menu created with a MenuItem and a FunctionItem and a CommandItem and a selection SubmenuItem')
+def step_impl(context):
+    context.menu = CursesMenu("Test Menu", "Subtitle")
+    context.menu_item = MenuItem("NORMAL", "Menu Item")
+    context.menu.append_item(context.menu_item)
+    context.function_item = FunctionItem("NORMAL", "Call a Python function", input, ["Enter an input"])
+    context.menu.append_item(context.function_item)
+    context.command_item = CommandItem("NORMAL", "Run a console command",  "ipconfig /all")
+    context.menu.append_item(context.command_item)
+    context.selection_menu = SelectionMenu(["item1", "item2", "item3"])
+    context.selection_submenu_item = SubmenuItem("NORMAL", "Submenu item", context.selection_menu)
+    context.menu.append_item(context.selection_submenu_item)
     pass
 
 
 @when("we implement a picker SubmenuItem")
 def step_impl(context):
-    context.picker_submenu_item = SubmenuItem("NORMAL", "Submenu item", context.picker_menu, context.menu)
+    context.picker_menu = PickerMenu(["item1", "item2", "item3"])
+    context.picker_submenu_item = SubmenuItem("NORMAL", "Submenu item", context.picker_menu)
     context.menu.append_item(context.picker_submenu_item)
-    context.menu.draw()
     pass
-
-
-@then("we will have an empty curses menu with a title")
-def step_impl(context):
-    assert context.menu is not None
-    assert context.menu.is_alive()
-    assert context.menu.is_running()
-
-
-@then("we will have a populated PickerMenu")
-def step_impl(context):
-    assert context.picker_menu is not None
-    assert context.picker_menu.is_alive()
-    assert context.picker_menu.is_running()
-
-
-@then("we will have a populated SelectionMenu")
-def step_impl(context):
-    assert context.selection_menu is not None
-    assert context.selection_menu.is_alive()
-    assert context.selection_menu.is_running()
-
-
-@then("we will have a curse menu containing a MenuItem")
-def step_impl(context):
-    assert context.menu_item.__str__() == "Test Menu Menu Item"
-    assert context.menu.is_alive()
-    assert context.menu.is_running()
-
-
-@then("we will have a curse menu containing a MenuItem")
-def step_impl(context):
-    assert context.menu_item.__str__() == "Test Menu Menu Item"
-    assert context.menu.is_alive()
-    assert context.menu.is_running()
-
-
-@then("we will have a curse menu containing a FunctionItem")
-def step_impl(context):
-    assert context.menu.is_alive()
-    assert context.menu.is_running()
-
-
-@then("we will have a curse menu containing a CommandItem")
-def step_impl(context):
-    assert context.menu.is_alive()
-    assert context.menu.is_running()
-
-
-@then("we will have a curse menu containing a SubmenuItem that contains a SelectionMenu")
-def step_impl(context):
-    assert context.menu.is_alive()
-    assert context.menu.is_running()
 
 
 @then("we will have a curse menu containing a SubmenuItem that contains a PickerMenu")
 def step_impl(context):
-    assert context.menu.is_alive()
-    assert context.menu.is_running()
+    assert context.menu is not None
+    assert context.menu_item is not None
+    assert context.function_item is not None
+    assert context.command_item is not None
+    assert context.selection_menu is not None
+    assert context.selection_submenu_item is not None
+    assert context.picker_menu is not None
+    assert context.picker_submenu_item is not None
