dcad19410e19e5350339ca95ea708382f53bbc49
michael565x <michael565x@gmail.com>
2017-05-04 10:53:20 -0400

    Fix feature and step files for mapping input feature

--- Mapping Inputs Using Dictionary.feature
+++ Mapping Inputs Using Dictionary.feature
@@ -4,52 +4,52 @@ Feature: Mapping Inputs Using Dictionary
   # The program will map user inputs by comparing them
   # to words in the dictionary.
 
-  Scenario: Input is in the dictionary.
+  Scenario: The given input is in the dictionary.
     Given The input is in the dictionary
-    When The user enters an input
+    When The user runs the menu
     Then The input should be mapped
 
-  Scenario: Input is not in the dictionary.
+  Scenario: The given input is not in the dictionary.
     Given The input is not in the dictionary
-    When The user enters an input
+    When The user runs curses menu
     Then The input will not be mapped.
 
   Scenario: One letter input in the dictionary.
     Given The input is a one letter word in the dictionary
-    When The user enters an input
-    Then The input should be mapped.
+    When The user uses the menu
+    Then The input will be mapped
 
   Scenario: One letter input not in the dictionary
     Given The input is a one letter word not in the dictionary.
-    When The user enters an input
-    Then The input will not be mapped.
+    When The user uses the curses menu
+    Then The input should not be mapped
 
   Scenario: Input is a Homograph.
     Given The input is a homograph in the dictionary
-    When The user enters an input
-    Then The input should be mapped
+    When The user starts the curses menu
+    Then The input should map
 
   Scenario: Input is a Heteronym
     Given the input is a heteronym in the dictionary
-    When The user enters an input
-    Then the input should be mapped
+    When The user starts the menu
+    Then the input is mapped
 
   Scenario: Input is a Homonym
     Given The input is a homonym in the dictionary
-    When The user enters an input
-    Then the input should be mapped
+    When The user runs curses
+    Then the input is then mapped
 
   Scenario: The input is misspelled
     Given The input is a misspelled word
-    When The user enters an input
-    Then the input will not be mapped
+    When The user starts the program
+    Then the input is not mapped
 
   Scenario: The input is not an English word.
     Given The input is not a word in the English language
-    When The user enters an input
-    Then the input will not be mapped
+    When The user runs the program
+    Then the input isnt mapped
 
-  Scenario: The input is a number
+  Scenario: The given input is a number
     Given The input is a number
-    When The user enters an input
-    Then The input will not be mapped
+    When The user starts up curses menu
+    Then The input is not to be mapped
--- steps/MapInputs_steps.py
+++ steps/MapInputs_steps.py
@@ -13,7 +13,7 @@ def step_impl(context):
     """
     pass
 
-@when("The user enters an input")
+@when("The user runs the menu")
 def step_impl(context, string):
     """
      :type string: input
@@ -21,6 +21,7 @@ def step_impl(context, string):
     """
     d = enchant.Dict("en_US")
     assert d.check(string)
+    pass # the menu has words in dictionary
 
 @then("The Input Should be mapped")
 def step_impl(context):
@@ -28,6 +29,7 @@ def step_impl(context):
     :type context behave.runner.Context
     """
     input.map()
+    pass # input is mapped
 
 
 @given("The input is not in the dictionary")
@@ -37,7 +39,7 @@ def step_impl(context):
     """
     pass
 
-@when("The user enters an input")
+@when("The user runs curses menu")
 def step_impl(context, string):
     """
     :type string: input
@@ -45,6 +47,7 @@ def step_impl(context, string):
     """
     d = enchant.Dict("en_US")
     assert d.check(string)
+    pass # any input not in the dictionary
 
 @then("The input will not be mapped")
 def step_impl(context):
@@ -52,7 +55,7 @@ def step_impl(context):
     :type context behave.runner.Context
     """
     input.map()
-
+    pass # input not mapped
 
 @given("The input is a one letter word in the dictionary")
 def step_impl(context):
@@ -61,7 +64,7 @@ def step_impl(context):
     """
     pass
 
-@when("The user enters an input")
+@when("The user uses the menu")
 def step_impl(context, string):
     """
     :type string: input
@@ -69,13 +72,15 @@ def step_impl(context, string):
     """
     d = enchant.Dict("en_US")
     assert d.check(string)
+    pass #input is one letter word in dictionary
 
-@then("The input should be mapped")
+@then("The input will be mapped")
 def step_impl(context):
     """
     :type context behave.runner.Context
     """
     input.map()
+    pass #input is mapped
 
 @given("The input is a one letter word not in the dictionary")
 def step_impl(context):
@@ -84,7 +89,7 @@ def step_impl(context):
     """
     pass
 
-@when("The user enters an input")
+@when("The user uses the curses menu")
 def step_impl(context, string):
     """
     :type string: input
@@ -92,12 +97,14 @@ def step_impl(context, string):
     """
     d = enchant.Dict("en_US")
     assert d.check(string)
+    pass #input is one letter word not in dictionary
 
-@then("The input will not be mapped")
+@then("The input should not be mapped")
 def step_impl(context):
     """
     :type context behave.runner.Context
     """
+    pass #input not mapped
 
 
 @given("The input is a Homograph")
@@ -107,7 +114,7 @@ def step_impl(context):
     """
     pass
 
-@when("The user enters an input")
+@when("The user starts the curses menu")
 def step_impl(context, string):
     """
     :type string: input
@@ -115,13 +122,15 @@ def step_impl(context, string):
     """
     d = enchant.Dict("en_US")
     assert d.check(string)
+    pass #input is a homograph in the dictionary
 
-@then("The input should be mapped")
+@then("The input should map")
 def step_impl(context):
     """
     :type context behave.runner.Context
     """
     input.map()
+    pass #input is mapped
 
 
 @given("The input is a Heteronym")
@@ -131,7 +140,7 @@ def step_impl(context):
     """
     pass
 
-@when("The user enters an input")
+@when("The user starts the menu")
 def step_impl(context, string):
     """
     :type string: input
@@ -139,13 +148,15 @@ def step_impl(context, string):
     """
     d = enchant.Dict("en_US")
     assert d.check(string)
+    pass #input is a heteronym in dictionary
 
-@then("The input should be mapped")
+@then("The input is mapped")
 def step_impl(context):
     """
     :type context behave.runner.Context
     """
     input.map()
+    pass #input is mapped
 
 
 @given("The input is a Homonym")
@@ -154,7 +165,8 @@ def step_impl(context):
     :type context behave.runner.Context
     """
     pass
-@when("The user enters an input")
+
+@when("The user runs curses")
 def step_impl(context, string):
     """
     :type string: input
@@ -162,13 +174,15 @@ def step_impl(context, string):
     """
     d = enchant.Dict("en_US")
     assert d.check(string)
+    pass #input is a homonym in dictionary
 
-@then("The input should be mapped")
+@then("The input is then mapped")
 def step_impl(context):
     """
     :type context behave.runner.Context
     """
     input.map()
+    pass #input is mapped
 
 
 @given("The input is a misspelled word")
@@ -178,7 +192,7 @@ def step_impl(context):
     """
     pass
 
-@when("The user enters an input")
+@when("The user starts the program")
 def step_impl(context, string):
     """
     :type string: input
@@ -186,12 +200,14 @@ def step_impl(context, string):
     """
     d = enchant.Dict("en_US")
     assert d.check(string)
+    pass #input is a misspelled word not in dictionary
 
-@then("The input will not be mapped")
+@then("The input is not mapped")
 def step_impl(context):
     """
     :type context behave.runner.Context
     """
+    pass #input is not mapped
 
 
 @given("The input is not a word in the English language")
@@ -200,7 +216,8 @@ def step_impl(context):
     :type context behave.runner.Context
     """
     pass
-@when("The user enters an input")
+
+@when("The user runs the program")
 def step_impl(context, string):
     """
     :type string: input
@@ -208,13 +225,14 @@ def step_impl(context, string):
     """
     d = enchant.Dict("en_US")
     assert d.check(string)
+    pass #input is not word in english dictionary
 
-@then("The input will not be mapped")
+@then("The input isnt mapped")
 def step_impl(context):
     """
     :type context behave.runner.Context
     """
-
+    pass #input is not mapped
 
 @given("The input is a number")
 def step_impl(context):
@@ -222,16 +240,19 @@ def step_impl(context):
     :type context behave.runner.Context
     """
     pass
-@when("The user enters an input")
+
+@when("The user starts up curses menu")
 def step_impl(context, string):
     """
     :type int: input
     :type context behave.runner.Context 
     """
     assert re.match("^[0-9 ]+$", string)
+    pass #the input is a number not in the dictionary
 
-@then("The input will not be mapped")
+@then("The input is not to be mapped")
 def step_impl(context):
     """
     :type context behave.runner.Context
     """
+    pass #input is not mapped
